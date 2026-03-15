464ec87fe29a0069ba0567481b2f0dacbe171bec
Azkizhraa <azkizhraa@gmail.com>
2026-03-15 22:37:43 +0700

    update flutter stateless attempt

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
+class MyCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const MyCard({super.key, required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(subtitle),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("my first app"),
+        centerTitle: true,
+        backgroundColor: Colors.amber[200]
+      ),
+      body: Center(
+        child: Text(
+        'Hello world!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'Poppins',
+        )
+        )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () { },
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+      ),
     ),
-    body: Center(
-      child: Text('hello ninjas!'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child: Text('click'),
-    )
-  ),
-));
+  ));
+}
